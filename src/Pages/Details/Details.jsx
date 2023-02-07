b6778152d2fe9288543285c0e76573cfd2c6d179
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-02-07 18:47:41 -0300

    continuo amanha

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,32 +1,41 @@
 import Data from "../../Data/data.json";
 
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function Details() {
   const { id } = useParams();
-  console.log(id);
 
   return (
     <div>
-      {Data.filter((code) => code.numericCode === id).map((item, index) => (
-        <div key={index}>
-          <img style={{ maxWidth: "200px" }} src={item.flags.svg} alt="Flags" />
-          <div>
-            <h3>{item.name}</h3>
-            <p>Population: {parseInt(item.population)}</p>
-            <p>Region: {item.region}</p>
-            <p>Capital: {item.capital}</p>
+      <Link to="/">Back</Link>
+
+      {Data.filter((code) => code.numericCode === id).map((item, index) => {
+        const population = item.population.toLocaleString("pt-br");
+
+        return (
+          <div key={index}>
+            <img
+              style={{ maxWidth: "200px" }}
+              src={item.flags.svg}
+              alt="Flags"
+            />
+            <div>
+              <h3>{item.name}</h3>
+              <p>Population: {population}</p>
+              <p>Region: {item.region}</p>
+              <p>Capital: {item.capital}</p>
+            </div>
+            <div>
+              <p>Top Level Domain: </p>
+              <p>Currencies: </p>
+              <p>Languages: </p>
+            </div>
+            <div>
+              <h3>Border Countries: </h3>
+            </div>
           </div>
-          <div>
-            <p>Top Level Domain: </p>
-            <p>Currencies: </p>
-            <p>Languages: </p>
-          </div>
-          <div>
-            <h3>Border Countries: </h3>
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Data from "../../Data/data.json";
 import "./home.css";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // const top20 = Data.slice(0, 10);
 
 function Home() {
+  const navigate = useNavigate();
+
+  function linkNavigate(code) {
+    navigate(`details/${code}`);
+  }
+
   return (
     <div className="Home">
       <div className="headerSearch">
@@ -19,18 +25,25 @@ function Home() {
         </select>
       </div>
       <section className="containerCountries">
-        {Data.filter((c) => c.population >= 112559400).map((item, index) => (
-          <div key={index} className="card">
-            <img src={item.flags.svg} alt="Flags" />
-            <div className="apiContentInfo">
-              <h2>{item.name}</h2>
-              <p>Population: {item.population}</p>
-              <p>Region: {item.region}</p>
-              <p>Capital: {item.capital}</p>
+        {Data.filter((c) => c.population >= 112559400).map((item, index) => {
+          const population = item.population.toLocaleString("pt-br");
+
+          return (
+            <div
+              key={index}
+              className="card"
+              onClick={() => linkNavigate(item.numericCode)}
+            >
+              <img src={item.flags.svg} alt="Flags" />
+              <div className="apiContentInfo">
+                <h2>{item.name}</h2>
+                <p>Population: {population}</p>
+                <p>Region: {item.region}</p>
+                <p>Capital: {item.capital}</p>
+              </div>
             </div>
-            <Link to={`details/${item.numericCode}`}> link</Link>
-          </div>
-        ))}
+          );
+        })}
       </section>
     </div>
   );
