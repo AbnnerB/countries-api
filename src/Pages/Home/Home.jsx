190d9db33dd43c0c897a4929a4cae52a7f62fa99
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-02-08 16:53:40 -0300

    mostrando os paises através dos nomes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,18 @@ import "./home.css";
 
 import { useNavigate } from "react-router-dom";
 import useModeContext from "../../hook/useModeContext";
+import { BsSearch } from "react-icons/bs";
+import { useState } from "react";
 
 // const top20 = Data.slice(0, 10);
+
+//brazil into data === line 2047
 
 function Home() {
   const navigate = useNavigate();
   const { darkMode } = useModeContext();
+
+  const [valueInputTextSearch, setValueInputTextSearch] = useState("");
 
   function linkNavigate(code) {
     navigate(`details/${code}`);
@@ -17,7 +23,18 @@ function Home() {
   return (
     <main className={darkMode} style={{ minHeight: "100vh" }}>
       <div className="headerSearch">
-        <input type="text" name="" id="" />
+        <div className="inputAndSearch">
+          <label className="search">
+            <BsSearch />
+          </label>
+          <input
+            placeholder="Search for a country"
+            type="text"
+            value={valueInputTextSearch}
+            onChange={(e) => setValueInputTextSearch(e.target.value)}
+            style={{ backgroundColor: darkMode === "darkMode" ? "" : "white" }}
+          />
+        </div>
         <select>
           <option value="">Filter by region</option>
           <option value="">Africa</option>
@@ -27,39 +44,80 @@ function Home() {
           <option value="">Oceania</option>
         </select>
       </div>
-      <section className="containerCountries">
-        {Data.filter((c) => c.population >= 112559400).map((item, index) => {
-          const population = item.population.toLocaleString("pt-br");
+      {valueInputTextSearch.length === 0 ? (
+        <section className="containerCountries">
+          {Data.filter((c) => c.region === "Africa").map((item, index) => {
+            const population = item.population.toLocaleString("pt-br");
 
-          return (
-            <div
-              key={index}
-              className="card"
-              style={{
-                backgroundColor: darkMode === "lightMode" ? "white" : "",
-              }}
-              onClick={() => linkNavigate(item.numericCode)}
-            >
-              <img
+            return (
+              <div
+                key={index}
+                className="card"
                 style={{
-                  borderBottom:
-                    darkMode === "lightMode"
-                      ? "1px solid rgb(214, 214, 214)"
-                      : "",
+                  backgroundColor: darkMode === "lightMode" ? "white" : "",
                 }}
-                src={item.flags.svg}
-                alt="Flags"
-              />
-              <div className="apiContentInfo">
-                <h2>{item.name}</h2>
-                <p>Population: {population}</p>
-                <p>Region: {item.region}</p>
-                <p>Capital: {item.capital}</p>
+                onClick={() => linkNavigate(item.numericCode)}
+              >
+                <img
+                  style={{
+                    borderBottom:
+                      darkMode === "lightMode"
+                        ? "1px solid rgb(214, 214, 214)"
+                        : "",
+                  }}
+                  src={item.flags.svg}
+                  alt="Flags"
+                />
+                <div className="apiContentInfo">
+                  <h2>{item.name}</h2>
+                  <p>Population: {population}</p>
+                  <p>Region: {item.region}</p>
+                  <p>Capital: {item.capital}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </section>
+            );
+          })}
+        </section>
+      ) : (
+        <section className="containerCountries">
+          {Data.filter(
+            (country) =>
+              country.name.toLowerCase() ===
+                valueInputTextSearch.toLowerCase() ||
+              country.alpha2Code === valueInputTextSearch.toUpperCase()
+          ).map((item, index) => {
+            const population = item.population.toLocaleString("pt-br");
+
+            return (
+              <div
+                key={index}
+                className="card"
+                style={{
+                  backgroundColor: darkMode === "lightMode" ? "white" : "",
+                }}
+                onClick={() => linkNavigate(item.numericCode)}
+              >
+                <img
+                  style={{
+                    borderBottom:
+                      darkMode === "lightMode"
+                        ? "1px solid rgb(214, 214, 214)"
+                        : "",
+                  }}
+                  src={item.flags.svg}
+                  alt="Flags"
+                />
+                <div className="apiContentInfo">
+                  <h2>{item.name}</h2>
+                  <p>Population: {population}</p>
+                  <p>Region: {item.region}</p>
+                  <p>Capital: {item.capital}</p>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      )}
     </main>
   );
 }
