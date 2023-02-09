0289549ab52540b0f1a8f6a89151b7a0c1b6056c
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-02-09 17:39:52 -0300

    alterando msg de aviso sobre o comopente footer
    o ponto e virgula estava no provider

--- src/components/FooterAllPage.jsx
+++ src/components/FooterAllPage.jsx
@@ -3,9 +3,9 @@ import "./footerStyle.css";
     `Oi, sim você mesmo que está inspecionando esse codigo,
 deve estar se perguntando o pq desse footer sem nada,
 a resposta é simples, na criação do dark mode,tive que lidaar com 
-um problema (e sim essa foi a melhor solução que eu consegui pensar).
+um problema (e sim essa foi a melhor solução que eu consegui pensar kkkk).
     O problema era que eu alterava a cor da minha pagina com o botão do header no darkMode
-e o root permanecia  com a cor branca, ficando uma borda de 10px no final da pagina 
+e o root permanecia  com a cor branca, ficando uma borda branca de 10px no final da pagina 
  ` */
 
 import useModeContext from "../hook/useModeContext";
